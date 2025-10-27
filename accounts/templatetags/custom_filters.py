0f0d3c8ad8f7f2a82bfa13b6670b1e2a3cff45fe
mCglawyer <emirsalihgogebakan@gmail.com>
2025-10-27 13:29:36 +0300

    Add templatetags folder and custom_filters

--- accounts/templatetags/custom_filters.py
+++ accounts/templatetags/custom_filters.py
@@ -1,5 +1,4 @@
 # accounts/templatetags/custom_filters.py
-
 from django import template
 from django.contrib.auth.models import Group
 
@@ -7,20 +6,10 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
-    """
-    Kullanıcının belirli bir grupta olup olmadığını kontrol eder.
-    Kullanım: {% if user|has_group:"Grup Adı" %}
-    """
     return user.groups.filter(name=group_name).exists()
 
 @register.filter(name='get_item')
 def get_item(dictionary, key):
-    """
-    Template içinde sözlükten değişkene göre anahtar almayı sağlar.
-    (weekly_schedule.html için gerekiyordu)
-    """
-    if not isinstance(dictionary, dict):
-        return None
-    if isinstance(key, int): # Anahtar sayıysa string'e çevir
-        key = str(key)
+    if not isinstance(dictionary, dict): return None
+    if isinstance(key, int): key = str(key)
     return dictionary.get(key)
